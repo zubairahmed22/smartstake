76b40e78a3ff40f631d8717153ee683cf580720a
unknown <zubairwastibaloch@gmail.com>
2023-11-05 10:51:34 -0800

    wagmi error fixed

--- src/App.js
+++ src/App.js
@@ -11,39 +11,36 @@ import BuyToken from "./Component/BuyToken";
 import Bio from "./bio"
 
 
-import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
-import { WagmiConfig } from 'wagmi'
-import { arbitrum, mainnet } from 'wagmi/chains'
+
+import { WagmiConfig, createConfig, configureChains, mainnet } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+
 
 
 
 
 function App() {
 
-  const projectId = '1242d9c1f7a1e32cc5fb0da6c6877f84'
+  
+  
+  const { chains, publicClient, webSocketPublicClient } = configureChains(
+    [mainnet],
+    [publicProvider()],
+  )
+   
+  const config = createConfig({
+    autoConnect: true,
+    publicClient,
+    webSocketPublicClient,
+  })
 
-  
-
-  // 2. Create wagmiConfig
-  const metadata = {
-    name: 'Web3Modal',
-    description: 'Web3Modal Example',
-    url: 'https://web3modal.com',
-    icons: ['https://avatars.githubusercontent.com/u/37784886']
-  }
-  
-  const chains = [mainnet, arbitrum]
-  const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
-  
-  // 3. Create modal
-  createWeb3Modal({ wagmiConfig, projectId, chains })
   
   
   
 
 
   return (
-    <WagmiConfig config={wagmiConfig}>
+    <WagmiConfig config={config}>
     <div className="App">
        {/* <div className="preloader">
         <div className="preloader__inner">
